1664acd4eaf49c3a21651b8e9033072798e60cd0
Shivam <shivamtamboli66@gmail.com>
2025-01-08 10:57:11 +0530

    get the employess details by their id

---
+++ src/main/java/net/javaguides/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,49 @@
+package net.javaguides.springbootbackend.controller;
+
+import net.javaguides.springbootbackend.exception.ResourceNotFoundException;
+import net.javaguides.springbootbackend.model.Employee;
+import net.javaguides.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
+@CrossOrigin("*")
+@RestController//spring mvc, handle http request
+//api's will define in this class.
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+
+    @Autowired//inserts the object of EmployeeRepository into the class.
+    private EmployeeRepository employeeRepository;//saving, fetching,interacting with database
+
+    @GetMapping// Maps the HTTP GET request to this method.
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    //build create employee REST API
+    @PostMapping// used to create new data in the database.
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+        //The client  sends data to the server, and data is json format.
+        //@RequestBody converts that JSON data into a Java object for processing.
+        // The method works with the Java object (e.g., saving it to the database).
+    }
+
+    //build create employee id by REST API
+    @GetMapping("{id}")//binds value from url (id) to method para (long id).
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("Employee doesn't exist with this id:" + id ));
+        return ResponseEntity.ok(employee);
+       //ResponseEntity wrapper class, clients get always clear response.<Type of data>,
+        //holds http responds.
+    }
+
+
+
+}
